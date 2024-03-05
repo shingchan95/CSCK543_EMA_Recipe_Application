559da47d420ff0f8a1a232050cd84e44ca761a10
Miguel Valadas <airesvaladas@gmail.com>
2024-03-05 17:54:35 +0000

    add environment variables support for database connection

--- src/config/database.php
+++ src/config/database.php
@@ -1,15 +1,16 @@
 <?php
 
-$host = "localhost"; // Your MySQL host
-$username = "root"; // Your MySQL username
-$password = ""; // Your MySQL password
-$database = "recipedb"; // Your MySQL database name
+// MySQL details with fallbacks for unset values
+$host = getenv("DB_HOST") ?: "127.0.0.1";
+$username = getenv("DB_USER") ?: "root";
+$password = getenv("DB_PASSWORD") ?: "";
+$database = "recipedb";
+$port = 3306; // Do not pass a port argument if you are not running your own MYSQL server apart from XAMPP
 
 // Create a connection to the database
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $database, $port);
 
 // Check the connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-?>
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
