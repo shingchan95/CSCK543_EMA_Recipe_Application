e2bcdcd5fa4efa24e54570b8e10af0e72de3ab54
Miguel Valadas <airesvaladas@gmail.com>
2024-03-10 10:09:28 +0000

    add base structure for homepage

---
+++ src/view/component/header.php
@@ -0,0 +1,17 @@
+<header>
+    <section>
+        <img>
+        <section>
+            <!-- Free text search -->
+        </section>
+    </section>
+
+    <nav>
+        <ul>
+            <!-- Nav Menu -->
+        </ul>
+        <section>
+            <!-- Login/Register -->
+        </section>
+    </nav>
+</header>
