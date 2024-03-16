7a2acbfb0e4e8a431777979f7b39bc4b8a3c03ad
Daniel T Davis <larlist@hotmail.com>
2024-03-16 14:10:34 +0000

    Removing redundant files

--- ImageUpdate.sql
+++
@@ -1,8 +0,0 @@
-UPDATE recipedb.recipe SET image_path = 'spaghetti_bolognese.jpg' WHERE id = 1;
-UPDATE recipedb.recipe SET image_path = 'vegan_american_pancakes.jpg' WHERE id = 2;
-UPDATE recipedb.recipe SET image_path = 'healthy_pizza.jpg' WHERE id = 3;
-UPDATE recipedb.recipe SET image_path = 'easy_lamb_biryani.jpg' WHERE id = 4;
-UPDATE recipedb.recipe SET image_path = 'couscous_salad.jpg' WHERE id = 5;
-UPDATE recipedb.recipe SET image_path = 'plum_clafoutis.jpg' WHERE id = 6;
-UPDATE recipedb.recipe SET image_path = 'mango_pie.jpg' WHERE id = 7;
-UPDATE recipedb.recipe SET image_path = 'mushroom_doner.jpg' WHERE id = 8;
