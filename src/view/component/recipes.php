e32f9cc1a5fd3cb61d68c4d1ec1878a2c7dd5d08
Miguel Valadas <airesvaladas@gmail.com>
2024-03-17 21:03:53 +0000

    reformat dates

--- src/view/component/recipes.php
+++ src/view/component/recipes.php
@@ -6,7 +6,7 @@ function displayRecipes($recipes, $addRemoveButton)
     foreach ($recipes as $recipe) {
         echo '<div class="card" onclick="goTo(\'/CSCK543_EMA_Recipe_Application/recipe\', ' . htmlspecialchars($recipe['id']) . ')">';
         if ($addRemoveButton) {
-            echo '<button class="remove_favorite_btn" data-recipe-id="' . htmlspecialchars($recipe['id']) . '">X</button>';
+            echo '<button class="remove_favorite_btn" data-recipe-id="' . htmlspecialchars($recipe['recipe_id']) . '">X</button>';
         }
         echo '<h3>' . htmlspecialchars($recipe['recipe']) . '</h3>';
         echo '<img class="card_image" src="/image/600/' . htmlspecialchars($recipe['image_path']) . '" alt="Recipe Image">';
@@ -15,7 +15,9 @@ function displayRecipes($recipes, $addRemoveButton)
         echo '<p><b>Preparation: </b>' . htmlspecialchars($recipe['preparation']) . ' minutes</p>';
         echo '<p><b>Cooking: </b>' . htmlspecialchars($recipe['cooking']) . ' minutes</p>';
         echo '<p><b>Author: </b>' . htmlspecialchars($recipe['author']) . '</p>';
-        echo '<p><b>Added: </b>' . htmlspecialchars($recipe['added']) . '</p>';
+        $date = date_create($recipe['added']);
+        // Formatting the Date into dd/mm/yyyy
+        echo '<p><b>Added: </b>' . htmlspecialchars(date_format($date, "d/m/Y")) . '</p>';
         echo '</div>';
     }
     echo '</div>';
